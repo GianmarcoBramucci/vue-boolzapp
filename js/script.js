2e77fe0007e1bf2bf35f7eadb5cacba7c3be60de
GianmarcoBramucci <giangian797@gmail.com>
2024-04-03 15:47:51 +0200

    setup

---
+++ js/script.js
@@ -0,0 +1,19 @@
+import {contacts} from "./data.js"; 
+
+Vue.createApp({
+    data(){
+        return{
+            contacts,
+             
+        }
+    },
+    methods: {
+
+    },
+    computed:{
+       
+    },
+    mounted(){
+       
+    }
+}).mount('#app')
