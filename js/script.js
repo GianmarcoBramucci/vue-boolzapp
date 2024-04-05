f5ff9cfe5e18e784584a00676a4e0a6581a13d69
GianmarcoBramucci <giangian797@gmail.com>
2024-04-05 10:51:24 +0200

    add active part

--- js/script.js
+++ js/script.js
@@ -4,20 +4,27 @@ Vue.createApp({
     data(){
         return{
             contacts,
-            atcvieContact: contacts[0] 
+            atcvieContact:{}
         }
     },
     methods: {
-
+        atcvieClass: function(object){
+            if(object.id==this.atcvieContact.id){
+                return 'active';
+            }    
+        },
+        changeAtcvieClass: function(object){
+           this.atcvieContact= object;    
+        }
     },
     computed:{
-       
     },
     mounted(){
         let i = 0;
-        for(let contact of contacts ){
+        for(let contact of this.contacts ){
             i++;
             contact.id=i+1;
         }
+        this.atcvieContact= contacts[0];
     }
 }).mount('#app')
